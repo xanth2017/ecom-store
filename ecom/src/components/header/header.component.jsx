3252ee18aa7255fd47677670bdf6105f5548f823
SW <xanth.t2017@gmail.com>
2019-10-03 18:39:32 +0800

    complete the sign-in

--- ecom/src/components/header/header.component.jsx
+++ ecom/src/components/header/header.component.jsx
@@ -6,7 +6,7 @@ import { auth } from '../../firebase/firebase.utils';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
-import { auth } from 'firebase';
+
 
 const Header = ({ currentUser }) => (
   <div className='header'>
--- ecom/src/components/sign-in/sign-in.component.jsx
+++ ecom/src/components/sign-in/sign-in.component.jsx
@@ -50,10 +50,12 @@ class SignIn extends React.Component {
             label='password'
             required
           />
+          <div className='buttons'>
           <CustomButton type='submit'> Sign in </CustomButton>
           <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
               Sign in with Google
           </CustomButton>
+          </div>
         </form>
       </div>
     );
